b6b9fd33e54c609bc16fb02d3ef02903f2e1152b
Graebor <graemeborland@gmail.com>
2026-02-20 16:19:37 -0600

    items have power ratings

--- items/item.gd
+++ items/item.gd
@@ -1,13 +1,14 @@
 class_name Item
 extends RigidBody3D
 
+@export var power_segments: int = 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	call_deferred("_setup")
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if is_in_group("InHand"):
 		linear_velocity = Vector3.ZERO
 		angular_velocity = Vector3.ZERO
