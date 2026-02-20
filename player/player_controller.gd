2ed1e2f422b6b2ec6cd01b207384871ccc058cb1
spencermarr <slaughterism@hotmail.com>
2026-02-20 13:25:59 -0600

    initial player controller

---
+++ player/player_controller.gd
@@ -0,0 +1,36 @@
+extends RigidBody3D
+
+@export var speed: float = 1.0
+@export var damping: float = 2.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+func _physics_process(delta: float) -> void:
+	var direction: Vector3 = _get_input()
+	linear_damp = damping
+	apply_force(speed * direction)
+	
+
+func _get_input() -> Vector3:
+	var direction = Vector3.ZERO
+
+	if Input.is_action_pressed("ui_right"):
+		direction.x += 1
+	if Input.is_action_pressed("ui_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		direction.z += 1
+	if Input.is_action_pressed("ui_up"):
+		direction.z -= 1
+		
+	direction = direction.normalized()
+
+	return direction
