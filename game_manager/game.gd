e377f3f184e5dd438140b5f8e27e9acc3245599d
Graebor <graemeborland@gmail.com>
2026-02-20 20:45:34 -0600

    score screen

--- game_manager/game.gd
+++ game_manager/game.gd
@@ -1,17 +1,23 @@
 extends Node
+class_name Game
 
 @export var player_scene: PackedScene
 @export var lobby_scene: PackedScene
+@export var score_screen_scene: PackedScene
 @export var levels: Array[PackedScene]
 
 var _scores: Dictionary[PlayerContext, int]
 var _active_level_index: int = -1
 var _active_level: Node
 var _lobby: Lobby
+var _score_screen: ScoreScreen
+
 
 func _ready() -> void:
+	GameManager.game = self
 	_move_to_lobby()
 	HoleManager.round_finished.connect(_on_round_finished)
+
 
 func _move_to_lobby() -> void:
 	_active_level_index = -1
@@ -19,12 +25,14 @@ func _move_to_lobby() -> void:
 	add_child(_lobby)
 	_lobby.players_confirmed.connect(_on_players_confirmed)
 
+
 func _on_players_confirmed() -> void:
 	if (_lobby != null):
 		_lobby.queue_free()
 		_lobby = null
 		_scores.clear()
 		_begin_level(0)
+
 
 func _begin_level(index: int) -> void:
 	if (index < levels.size()):
@@ -44,6 +52,7 @@ func _begin_level(index: int) -> void:
 		print("all levels done, going to lobby")
 		_move_to_lobby()
 
+
 func _on_round_finished() -> void:
 	print("adding scores together")
 	for player: PlayerContext in PlayerManager.round_order.keys():
@@ -51,7 +60,18 @@ func _on_round_finished() -> void:
 			_scores[player] = PlayerManager.round_order[player]
 		else:
 			_scores[player] += PlayerManager.round_order[player]
+	
+	_score_screen = score_screen_scene.instantiate()
+	add_child(_score_screen)
+	_score_screen.finished_looking_at_scores.connect(_on_finished_looking_at_scores)
+
+	
+func _on_finished_looking_at_scores() -> void:
+	if (_score_screen != null):
+		_score_screen.queue_free()
+		_score_screen = null
 	_begin_level(_active_level_index + 1)
+
 
 func _clear_active_level() -> void:
 	if (_active_level != null):
--- game_manager/game_manager.gd
+++ game_manager/game_manager.gd
@@ -2,5 +2,7 @@ extends Node
 
 signal level_started
 
+var game: Game
+
 func notify_level_started() -> void:
 	level_started.emit()
