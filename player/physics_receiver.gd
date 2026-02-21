626484d9b896e351f0c97c43a0c5e0a5b4254172
spencermarr <slaughterism@hotmail.com>
2026-02-21 15:10:59 -0600

    receive impulse on collision with items

--- player/physics_receiver.gd
+++ player/physics_receiver.gd
@@ -1,13 +1,26 @@
 extends Area3D
 
-@export var velocity_threshold: float = 10
-@export var multiplier: float = 5.0
+@export var velocity_threshold: float = 5.0
+@export var multiplier: float = 40.0
 
 var player: PlayerController
+var locked_item: RigidBody3D
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 	player = get_parent()
+	player.swing_began.connect(_on_swing_began)
+	player.swing_ended.connect(_on_swing_ended)
+
+
+func _on_swing_began() -> void:
+	if player.swing_spot.get_child_count() > 0:
+		locked_item = player.swing_spot.get_child(0) 
+		print(locked_item.name)
+
+
+func _on_swing_ended() -> void:
+	locked_item = null
 
 
 func _on_body_entered(node: Node3D) -> void:
@@ -15,12 +28,16 @@ func _on_body_entered(node: Node3D) -> void:
 		return
 	
 	var body: RigidBody3D = node as RigidBody3D
-	#print("Angular: %s" % body.angular_velocity)
-	#print("Linear: %s" % body.linear_velocity)
-	#print("")
+	if locked_item != null and body == locked_item:
+		return
 	
 	if body.linear_velocity.length() > velocity_threshold or body.angular_velocity.length() > velocity_threshold:
-		player.apply_force(body.linear_velocity * multiplier)
+		var player_pos: Vector3 = player.global_position
+		var body_pos: Vector3 = body.global_position
+		player_pos.y = 0
+		body_pos.y = 0
+		
+		var direction: Vector3 = player_pos - body_pos
+		direction = direction.normalized()
+		player.apply_impulse(direction * body.linear_velocity.length() * multiplier)
 		player.stun(min(body.linear_velocity.length() / 10.0, 3.0))
-		
-		
