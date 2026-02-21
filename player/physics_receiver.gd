75d3c5da85ccd97e64ed1d12770c452ba6cec43d
spencermarr <slaughterism@hotmail.com>
2026-02-21 15:46:50 -0600

    drop items on stun

--- player/physics_receiver.gd
+++ player/physics_receiver.gd
@@ -5,6 +5,8 @@ extends Area3D
 
 var player: PlayerController
 var locked_item: RigidBody3D
+var locked_item_of_other_player: RigidBody3D 
+
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
@@ -15,8 +17,11 @@ func _ready() -> void:
 
 func _on_swing_began() -> void:
 	if player.swing_spot.get_child_count() > 0:
-		locked_item = player.swing_spot.get_child(0) 
-		print(locked_item.name)
+		locked_item = player.swing_spot.get_child(0)
+		if locked_item is PlayerController:
+			var other_player: PlayerController = locked_item as PlayerController
+			if other_player.inventory.get_child_count() > 0:
+				locked_item_of_other_player = other_player.inventory.get_child(0)
 
 
 func _on_swing_ended() -> void:
@@ -24,14 +29,18 @@ func _on_swing_ended() -> void:
 
 
 func _on_body_entered(node: Node3D) -> void:
-	if not node.is_in_group("Item") or node.is_in_group("Sinking") or node.is_in_group("InHand"):
+	if node.is_in_group("Sinking") or node.is_in_group("InHand"):
 		return
 	
 	var body: RigidBody3D = node as RigidBody3D
 	if locked_item != null and body == locked_item:
 		return
 	
+	if locked_item_of_other_player != null and body == locked_item_of_other_player:
+		return
+	
 	if body.linear_velocity.length() > velocity_threshold or body.angular_velocity.length() > velocity_threshold:
+		print(body.name)
 		var player_pos: Vector3 = player.global_position
 		var body_pos: Vector3 = body.global_position
 		player_pos.y = 0
