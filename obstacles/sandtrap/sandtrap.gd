edbed46da09943a8ab96ca79cc9550f858b9b1ea
spencermarr <slaughterism@hotmail.com>
2026-02-22 10:55:09 -0600

    improved level 1

--- obstacles/sandtrap/sandtrap.gd
+++ obstacles/sandtrap/sandtrap.gd
@@ -1,10 +1,14 @@
-extends Node
+@tool
+extends CSGPolygon3D
 
 @export var area: Area3D
 const SAND_GROUP: String = "Sand"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	if Engine.is_editor_hint():
+		global_position.y = -0.48
+	
 	area.body_entered.connect(_on_body_entered)
 	area.body_exited.connect(_on_body_exited)
 
