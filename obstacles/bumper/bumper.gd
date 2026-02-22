6b83b7235292ae62f7bdbfb380f438ea7242f5f2
Graebor <graemeborland@gmail.com>
2026-02-22 12:19:03 -0600

    bumper polish, collision interpolation on

--- obstacles/bumper/bumper.gd
+++ obstacles/bumper/bumper.gd
@@ -2,21 +2,24 @@
 extends RigidBody3D
 
 @export var radius: float = 2.0
+@export var scaler: Node3D
+@export var tweener: Node3D
+
+var _tween: Tween
 
 @onready var collider: CollisionShape3D = $CollisionShape3D
-@onready var shape: CSGCylinder3D = $CSGCylinder3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 	_resize()
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Engine.is_editor_hint():
 		_resize()
 
 func _resize() -> void:
-	shape.radius = radius
+	scaler.scale = Vector3.ONE * radius * 2.0
 	var collision_shape := collider.shape as CylinderShape3D
 	collision_shape.radius = radius
 
@@ -34,9 +37,21 @@ func _on_body_entered(node: Node3D) -> void:
 	
 	var direction: Vector3 = node_pos - self_pos
 	body.apply_impulse(max(body.linear_velocity.length(), 4.0) * direction * body.mass)
+	_do_tween(clamp(body.linear_velocity.length() / 4.0, 0.0, 1.0))
 	if not body is PlayerController:
 		return
 		
 	var player: PlayerController = body as PlayerController
 	player.stun(0.5)
-	
+
+func _do_tween(amount: float) -> void:
+	if (_tween != null):
+		_tween.kill()
+	_tween = get_tree().create_tween().set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
+	_tween.tween_property(tweener, "scale", Vector3.ONE * lerp(1.03, 1.2, amount), 0.05)
+	_tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BOUNCE)
+	_tween.tween_property(tweener, "scale", Vector3.ONE, lerp(0.2, 0.4, amount))
+	_tween.tween_callback(_on_tween_complete)
+
+func _on_tween_complete() -> void:
+	_tween = null
